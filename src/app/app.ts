f22621dd4e3e9d89272c3585b37f1ec48b9fb88d
DayanDev87 <Dev.thinkit@gmail.com>
2025-06-26 10:30:41 +0800

    1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+ngForm: any;
+
+getData(data:NgForm) {
+console.log(data.value);
+}
+
   protected title = 'OlloLifestyle.WebApp';
+  name = 'Ollo Lifestyle Web App';
+  display = true;
+
+  constructor() {
+    var name= 'Ollo Lifestyle Web App';
+    console.log(name);
+  }
+clickMe() {
+    console.log('Button clicked!');
+    alert('Button clicked!');
+  }
+
 }
